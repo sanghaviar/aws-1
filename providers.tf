4ba486dce5b815c395750ebb82255e91bd031823
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-05 18:01:13 +0530

    code update

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.33.0"
+    }
+    databricks = {
+      source  = "databricks/databricks"
+      version = ">=1.9.2"
+    }
+  }
+
+  backend "s3" {
+    bucket = "statefilebucketcubx"
+    region = "us-east-1"
+    key = "repo.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "CLIENT_ID1" {
+  type = string
+}
+
+variable "CLIENT_SECRET1" {
+  type = string
+}
+variable "region" {
+  default = "us-east-1"
+}
