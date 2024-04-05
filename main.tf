6c188382aafd592e4a31f6388e8e690ba5616660
sanghavi <sanghavi.a.r@diggibyte.com>
2024-04-05 18:48:55 +0530

    main

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ locals {
   data    = jsondecode(file("${path.module}/config/config.json"))
 }
 module "s3_bucket" {
-  source = "../modules/modules/s3"
+  source = "./modules/modules/s3"
   for_each      = {for entry in local.data["s3"] : entry["aws_s3_bucket_name"] => entry}
   config = each.value
 }
