c057a0591651c7e48fa850f0517b9904d27ff4de
sanghaviar <139112698+sanghaviar@users.noreply.github.com>
2024-09-17 22:35:18 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,7 +13,7 @@ terraform {
   backend "s3" {
     bucket = "statefilebucketcubx"
     region = "us-east-1"
-    key = "repo2.tfstate"
+    key = "repo3.tfstate"
     dynamodb_table = "test_dynamo111"
   }
 }
