2d690e58497e2cd4a372d8ee99ff47b8945ea3ed
sanghaviar <139112698+sanghaviar@users.noreply.github.com>
2024-09-17 23:03:12 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -28,16 +28,16 @@ provider "aws" {
 provider "databricks" {
   alias         = "mws"
   host          = "https://accounts.cloud.databricks.com"
-  access_key     = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+ access_key     = var.AWS_ACCESS_KEY_ID
+ secret_key = var.AWS_SECRET_ACCESS_KEY
   account_id = var.ACCOUNT_ID
 }
 provider "databricks" {
   alias = "accounts"
   host = "https://accounts.cloud.databricks.com"
-  #    client_id = "ff089d9f-ab47-4591-93d0-c9f603455839"
-  #    client_secret = " dose4cb65bc38bf2bee8c306865bfe455a22"
-  username = var.DATABRICKS_ACCOUNT_USERNAME
-  password = var.DATABRICKS_ACCOUNT_PASSWORD
+  client_id = var.DATABRICKS_CLIENT_ID
+  client_secret = var.DATABRICKS_CLIENT_SECRET
+ # username = var.DATABRICKS_ACCOUNT_USERNAME
+ # password = var.DATABRICKS_ACCOUNT_PASSWORD
   account_id = var.ACCOUNT_ID
 }
